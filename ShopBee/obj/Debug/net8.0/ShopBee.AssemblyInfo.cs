83e8d9e9370397465a5afd957299723eb39eea08
Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
2023-12-15 00:31:23 +0700

    Squashed commit of the following:
    commit 8ca2c1e88c785dcfebffe2ad8ba3c045dfc067ee
    Merge: 5baf0d4 3c1093b
    Author: Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
    Date:   Wed Dec 13 18:30:55 2023 +0700
    
        Merge pull request #15 from qngoc07012002/ttphuc
    
        Update UserRole Admin
    
    commit 3c1093b55144143dcb55bde75b468b2c821b7950
    Author: Truong Tan Phuc <phuccan0800@gmail.com>
    Date:   Wed Dec 13 15:22:06 2023 +0700
    
        fix bug table
    
    commit b1723c6c8ce39ff26725f1eeec62bc6f42e6f6fc
    Author: Truong Tan Phuc <phuccan0800@gmail.com>
    Date:   Wed Dec 13 15:08:50 2023 +0700
    
        Update UserRole CRUD

--- ShopBee/obj/Debug/net8.0/ShopBee.AssemblyInfo.cs
+++ ShopBee/obj/Debug/net8.0/ShopBee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopBee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9f86bf9382e04c2f1fe37cfb02deb5bf9c48f22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1723c6c8ce39ff26725f1eeec62bc6f42e6f6fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopBee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopBee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
