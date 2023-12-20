f6c83686eea2c1b0ddfabcefa526ac71c41e2087
Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
2023-12-21 01:20:17 +0700

    Update Product List Home Page, Category, Search, View Detail

---
+++ ShopBee/Areas/Customer/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopBee.Models;
+using ShopBee.Repository.IRepository;
+
+namespace ShopBee.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class BookController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookController(IUnitOfWork db)
+        {
+            _unitOfWork = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Book book = _unitOfWork.Book.Get(c=> c.Id == id);
+            return View(book);
+        }
+    }
+}
