e34e353f0674ec8f055f420c78bae7ed98f8aad4
Nguyen Quang Ngoc <116235732+qngoc07012002@users.noreply.github.com>
2023-12-22 20:05:08 +0700

    Update Store View

--- ShopBee/obj/Debug/net8.0/ShopBee.AssemblyInfo.cs
+++ ShopBee/obj/Debug/net8.0/ShopBee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopBee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb7c8c456f8b0351243e8c716ae044c5658e8b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05c872d470828b973c4e0ed6b6e4c69f51531e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopBee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopBee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
